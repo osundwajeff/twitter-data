078702254aa816493a95b82bbec77a9f9aca2e50
osundwajeff <mulamajeff@tuta.io>
2021-12-17 19:13:22 +0300

    twitter stuff

---
+++ nodejs-leaflet-eventstreams-master/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.port = 8000;
+config.address = 'localhost';
+config.twitter_username = process.env.TWITTER_USERNAME || '_jeff_';
+config.twitter_password = process.env.TWITTER_PASSWORD || '$xc9aP@Q!db!8@';
+
+module.exports = config;
